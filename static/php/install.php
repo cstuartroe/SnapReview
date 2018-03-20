6e25a3aea10ef6b0b3018a2c49ea30608ad4ea4f
root <cstuartroe@haverford.edu>
2018-03-20 03:35:22 +0000

    finish OAuth

--- static/php/install.php
+++ static/php/install.php
@@ -2,9 +2,9 @@
 
 // Set variables for our request
 $shop = $_GET['shop'];
-$api_key = "1r30mrvCFMfq2DLGuIXyY2veEJVgTtDD";
+$api_key = "97db2b13d9e38f92a5267e192d1a9ca4";
 $scopes = "read_orders,write_products";
-$redirect_uri = "http://localhost/generate_token.php";
+$redirect_uri = "http://snapreview.conorstuartroe.com/generate_token.php";
 
 // Build install/approval URL to redirect to
 $install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
---
+++ static/php/test.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<?php
+
+echo "test.php did one success!';
+
+?>
+</body>
+</html>
